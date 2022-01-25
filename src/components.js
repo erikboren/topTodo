ffd1487a4ac2985fd41ad01257446db9c830c2d0
erikboren <erik.boren@gmail.com>
2022-01-25 18:13:11 +0100

    Project and todo factories

---
+++ src/components.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+const navBarElement = function(){
+
+};
+
+const todoNavElement = function(todo){
+
+};
+
+const projectNavElement = function(project){
+
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
 /*jshint esversion: 6 */
-console.log("Hello World!");
 
-const title = document.createElement("h1");
+const todoDatabase = (function(){
+    let todoIDCounter = 0;
+    
+    const todoFactory = function(todoName,todoDate,todoDesc){
+        let name = todoName;
+        let date = todoDate;
+        
+        let desc = todoDesc;
+        let completed = false;
+        let project = "No Project";
+        let todoID = todoIDCounter;
+        let projectID = 0;
+        
+        todoIDCounter ++;
+       
+        return{
+            name: name,
+            date: date,
+            project: project,
+            desc: desc,
+            completed: completed,
+            todoID: todoID,
+            projectID: projectID
+        };
+       };
+       
+    
+    
+    const todoArray = [];
 
-title.textcontent = "Hejsan";
+    const addTodo = function(todoName,todoDate,todoDesc){
+        const todo = todoFactory(todoName,todoDate,todoDesc);
+        todoArray.push(todo);
+    };
 
-document.querySelector("body").appendChild(title);
+    return{
+        addTodo: addTodo,
+        todoArray:todoArray        
+    };
+
+})();
+
+const projectDatabase = (function(){
+    const projectFactory = function(projectName){
+        
+
+
+        const projectTodos = function(){
+
+        }
+
+        return{
+            name: projectName,
+        };
+    };
+    
+    const projectArray = [];
+
+    const addProject = function(projectName){
+        const project = projectFactory(projectName);
+        projectArray.push(project);
+    };
+
+   return{
+       addProject: addProject,
+       projectArray: projectArray
+   } ;
+})();
+
+
+
+projectDatabase.addProject("Projektet");
+
+todoDatabase.addTodo("namn","datum","beskrivning");
+
+console.log(todoDatabase.todoArray[0]);
