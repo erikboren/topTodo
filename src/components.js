8da4e0452372f35682391a6bf433c2b7b1b23240
erikboren <erik.boren@gmail.com>
2022-01-27 15:59:42 +0100

    Side bar project list

--- src/components.js
+++ src/components.js
@@ -1,17 +1,58 @@
 /*jshint esversion: 6 */
+import './style.css';
+export function sideBarComponentConstructor(){
+    function sideBarLogo(){
+        const logo = document.createElement("div");
+        logo.textContent = "topToDo";
+        logo.classList.add("sideBarLogo");
+        return logo;
+    }
 
-const navBarComponentConstructor = function(){
+    function sideBarTopLevelHeader(text,plusButton){
+        const header = document.createElement("div");
+        const headerText = document.createElement("p");
+        const headerPlusButton = document.createElement("button");
+        
+        headerText.textContent = text;
+        header.classList.add("sideBarTopLevelHeader");
 
-};
+        header.appendChild(headerText);
 
-const todoNavElementConstructor = function(todo){
+        headerPlusButton.classList.add("sideBarPlusButton");
+        headerPlusButton.textContent = "+";
 
-};
+        if(plusButton){
+            header.appendChild(headerPlusButton);
+        }
 
-const projectNavElementConstructor = function(project){
+        return header;
+    }
+    
+    const sideBarElement = document.createElement("div");
+    sideBarElement.classList.add("sideBar");
+    sideBarElement.appendChild(sideBarLogo());
+    sideBarElement.appendChild(sideBarTopLevelHeader("PROJECTS",true));
+    
+    return sideBarElement;
+}
 
-};
 
-const mainComponentConstructor = function(){
+export function projectSideBarElementConstructor(project){
+    const projectSideBarElement = document.createElement("p");
+    projectSideBarElement.textContent = project.name;
+    projectSideBarElement.classList.add("sideBarListElement");
+    
+    return projectSideBarElement;
+    
+}
 
-};
+export function mainWindowConstructor(){
+    const mainWindow = document.createElement('div');
+    mainWindow.classList.add("mainWindow");
+    return mainWindow;
+}
+
+export function todoListConstructor(){
+
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 /*jshint esversion: 6 */
+import './style.css';
+import { sideBarComponentConstructor } from './components.js';
+import { mainWindowConstructor } from './components.js';
+import { projectSideBarElementConstructor } from './components.js';
 
 const todoDatabase = (function(){
     let todoIDCounter = 0;
@@ -10,7 +14,7 @@ const todoDatabase = (function(){
         let desc = todoDesc;
         let completed = false;
         let todoID = todoIDCounter;
-        let projectID = [-1];
+        let projectID = [99];
         
         todoIDCounter ++;
        
@@ -43,7 +47,7 @@ const todoDatabase = (function(){
     
         if(projectDatabase.projectArray.some(project => project.projectID == projectIDToAssign)){
             const todoToEdit = todoArray.filter(todo => todo.todoID == todoID)[0];
-           if(todoToEdit.projectID == -1){
+           if(todoToEdit.projectID == [99]){
                 
                 todoToEdit.projectID = [projectIDToAssign];
 
@@ -70,6 +74,13 @@ const todoDatabase = (function(){
 
 const projectDatabase = (function(){
     let projectIDcounter = 0;
+    
+    function sortProjectArray(){
+        projectArray = projectArray.sort((a,b) =>{
+            return a.projectID - b.projectID;
+        });
+    }
+
     const projectFactory = function(projectName){
         
         let name = projectName;
@@ -89,12 +100,18 @@ const projectDatabase = (function(){
         };
     };
     
-    const projectArray = [];
+    let projectArray = [];
 
     const addProject = function(projectName){
         const project = projectFactory(projectName);
         projectArray.push(project);
+        sortProjectArray();
     };
+
+    addProject("Tasks not in a project");
+    projectArray[0].projectID = 99;
+    projectIDcounter = 0;
+
 
    return{
        addProject: addProject,
@@ -102,28 +119,54 @@ const projectDatabase = (function(){
    } ;
 })();
 
+const screenController = (function(){
+    const contentDiv = document.getElementById("content");
+    
+    
+
+    const buildPage = function(){
+        function sideBar(){
+            const sideBar = sideBarComponentConstructor();
+            projectDatabase.projectArray.forEach(project =>{
+                sideBar.appendChild(projectSideBarElementConstructor(project));
+            });
+            return sideBar;
+        }
+        contentDiv.appendChild(sideBar());
+        contentDiv.appendChild(mainWindowConstructor());
+    };
+
+    return {
+        buildPage: buildPage
+    };
+
+})();
+
+projectDatabase.addProject("Projekt1");
+projectDatabase.addProject("Projekt2");
+
+screenController.buildPage();
 
 
-projectDatabase.addProject("Projektet");
-projectDatabase.addProject("Projektet2");
-
-todoDatabase.addTodo("namn0","datum0","beskrivning0");
-todoDatabase.addTodo("namn1","datum1","beskrivning1");
-todoDatabase.addTodo("namn2","datum2","beskrivning2");
-todoDatabase.addTodo("namn3","datum3","beskrivning3");
-todoDatabase.addTodo("namn4","datum4","beskrivning4");
-todoDatabase.addTodo("namn5","datum5","beskrivning5");
-todoDatabase.addTodo("namn6","datum6","beskrivning6");
-todoDatabase.addTodo("namn7","datum8","beskrivning9");
 
 
-todoDatabase.assignProjectID(0,5);
-todoDatabase.assignProjectID(0,2);
-todoDatabase.assignProjectID(0,4);
-todoDatabase.assignProjectID(1,4);
+// todoDatabase.addTodo("namn0","datum0","beskrivning0");
+// todoDatabase.addTodo("namn1","datum1","beskrivning1");
+// todoDatabase.addTodo("namn2","datum2","beskrivning2");
+// todoDatabase.addTodo("namn3","datum3","beskrivning3");
+// todoDatabase.addTodo("namn4","datum4","beskrivning4");
+// todoDatabase.addTodo("namn5","datum5","beskrivning5");
+// todoDatabase.addTodo("namn6","datum6","beskrivning6");
+// todoDatabase.addTodo("namn7","datum8","beskrivning9");
+
+
+// todoDatabase.assignProjectID(0,5);
+// todoDatabase.assignProjectID(0,2);
+// todoDatabase.assignProjectID(0,4);
+// todoDatabase.assignProjectID(1,4);
 
 
 
-console.table(todoDatabase.todoArray);
+// console.table(todoDatabase.todoArray);
 
-console.log(projectDatabase.projectArray[0].projectTodos());
+// console.log(projectDatabase.projectArray[0].projectTodos());
